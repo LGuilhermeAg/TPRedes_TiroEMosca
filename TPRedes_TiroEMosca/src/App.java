2a38de4260154beec607799718b8cdfc1350bf06
Luis Guilherme <luisguilhermeti@gmail.com>
2022-10-25 19:21:51 -0300

    Primeiro input scanner :D

--- TPRedes_TiroEMosca/src/App.java
+++ TPRedes_TiroEMosca/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Insira o número palpite:");
+        Scanner scan = new Scanner(System.in);
+        int palpite = scan.nextInt();
+        // Math.floor(Math.random()*(max-min+1)+min) 
+        System.out.println("Voce digitou:"+palpite);
+        System.out.println("Insira o número palpite:");
+        palpite = scan.nextInt();
+        System.out.println("Voce digitou:"+palpite);
     }
 }
